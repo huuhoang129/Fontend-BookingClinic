acd60e23b0b33fdd86190458a5c972581cc27c59
Le Ngoc Hoang <dinoradio12z@gmail.com>
2025-04-13 19:23:52 +0700

    Contract Backend

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -5,7 +5,7 @@ import * as actions from "../../store/actions";
 
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
-
+import { handleLoginApi } from '../../services/userService';
 
 class Login extends Component {
     constructor(props) {
@@ -14,6 +14,7 @@ class Login extends Component {
             username: '',
             password: '',
             isShowPassword: false,
+            errMessage: ''
         }
     }
 
@@ -29,9 +30,30 @@ class Login extends Component {
         })
     }
 
-    handleLogin = () => {
-        console.log('username: ', this.state.username, 'password: ', this.state.password)
-        console.log('all state: ', this.state)
+    handleLogin = async () => {
+        this.setState({
+            errMessage: ''
+        })
+        try{
+            let data = await handleLoginApi(this.state.username, this.state.password);
+            if (data && data.errCode !== 0){
+                this.setState({
+                    errMessage: data.message
+                })
+            }
+            if (data && data.errCode === 0){
+                this.props.userLoginSuccess(data.user)
+                console.log('login succeeds')
+            }
+        }catch(error){
+            if (error.response) {
+                if (error.response.data) {
+                    this.setState({
+                        errMessage: error.response.data.message
+                    })
+                }
+            }
+        }
     }
 
     handleShowHidePassword = () => {
@@ -41,6 +63,7 @@ class Login extends Component {
     }
 
     render() {
+        //JSX
         return (
             <div className="login-background">
                 <div className="login-container">
@@ -68,6 +91,9 @@ class Login extends Component {
                                 ><i class={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
                                 </span>
                             </div>
+                        </div>
+                        <div className='col-12' style={{ color: 'red'}}>
+                            {this.state.errMessage}
                         </div>
                         <div className="col-12">
                             <button className='btn-login' onClick={()=> {this.handleLogin()}}>Login</button>
@@ -98,8 +124,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        // userLoginFail: () => dispatch(actions.adminLoginFail()),
+        userLoginSuccess: (userInfo) => dispatch(actions.userLoginSuccess(userInfo))
     };
 };
 
